0b8806562b5efa595d4da2192f98b35c143fba40
Jose <josegarciaemail96@gmail.com>
2024-02-12 20:06:50 -0500

    3done

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -22,7 +22,12 @@ console.log(result3); // hi!!!!!
 *******************************************************************************/
 
 function multiMap(val, n, cb) {
-  // Your code here 
+  let result = val;
+
+  for(let i = 0; i < n ;i++){
+      result = cb(result)
+  }
+      return result;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
