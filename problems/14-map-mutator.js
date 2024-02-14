f8f99df7b065cc8aac5af8b5bab58b3ae5a395dd
Jose <josegarciaemail96@gmail.com>
2024-02-13 20:32:57 -0500

    14 done

--- problems/14-map-mutator.js
+++ problems/14-map-mutator.js
@@ -19,7 +19,9 @@ console.log(arr2); // [ 0, 9, 20 ]
 *******************************************************************************/
 
 function mapMutator(array, cb) {
-  // Your code here 
+  for(let i = 0; i < array.length;i++){
+    array[i] = cb(array[i], i);
+  }
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
