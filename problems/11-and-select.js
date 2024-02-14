b718740639876e2a2bd822848a216dfed938b2ef
Jose <josegarciaemail96@gmail.com>
2024-02-13 19:30:44 -0500

    11done

--- problems/11-and-select.js
+++ problems/11-and-select.js
@@ -29,7 +29,14 @@ console.log(andSelect(['ants', 'APPLES', 'ART', 'BACON', 'arm'], isUpperCase,  s
 *******************************************************************************/
 
 function andSelect(array, cb1, cb2) {
-  // Your code here 
+  let newArray = [];
+
+  for(let i = 0; i < array.length;i++){
+    if(cb1(array[i]) && cb2(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+    return newArray;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
