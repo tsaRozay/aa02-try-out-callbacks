bacbdfd55be0081e4bc4037b669756d84031403f
Jose <josegarciaemail96@gmail.com>
2024-02-13 19:38:28 -0500

    12 done

--- problems/12-exactly.js
+++ problems/12-exactly.js
@@ -28,7 +28,14 @@ console.log(result4); // true
 *******************************************************************************/
 
 function exactly(array, num, cb) {
-  // Your code here 
+  let count = 0;
+
+  for(let i = 0; i < array.length;i++){
+    if(cb(array[i])){
+      count++;
+    }
+  }
+    return count === num;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
