cf5ca3ba89b8529e870669e21b5483a601cd42a0
Jose <josegarciaemail96@gmail.com>
2024-02-13 19:17:45 -0500

    9 done

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -30,7 +30,12 @@ console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
 function chainMap(val, ...callbacks) {
-  // Your code here 
+  let result = val;
+
+  for(let callback of callbacks){
+    result = callback(result);
+  }
+    return result;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
